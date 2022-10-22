0c97c986041cbdcb5e3d9ddddbdb4686ea5542ba
Thomas-Jourdain <79916850+Thomas-Jourdain@users.noreply.github.com>
2022-10-22 16:23:14 +0200

    Ajout du LightWall du joueur1
    Programmation de la trace du joueur1(joueur Cyan ).
    Pas de collision pour le moment

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -13,11 +13,44 @@ public class Move : MonoBehaviour
     // Movement Speed
     public float speed = 16;
 
+    // Wall Prefab
+    public GameObject wallPrefab;
+
+    // Current Wall
+    Collider2D wall;
+
+    // Last Wall's End
+    Vector2 lastWallEnd;
+
+    void spawnWall()
+    {
+        // Save last wall's position
+        lastWallEnd = transform.position;
+
+        // Spawn a new Lightwall
+        GameObject g = Instantiate(wallPrefab, transform.position, Quaternion.identity);
+        wall = g.GetComponent<Collider2D>();
+    }
+
+    void fitColliderBetween(Collider2D co, Vector2 a, Vector2 b)
+    {
+        // Calculate the Center Position
+        co.transform.position = a + (b - a) * 0.5f;
+
+        // Scale it (horizontally or vertically)
+        float dist = Vector2.Distance(a, b);
+        if (a.x != b.x)
+            co.transform.localScale = new Vector2(dist+1, 1);
+        else
+            co.transform.localScale = new Vector2(1, dist+1);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         // Initial Velocity
         GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+        spawnWall();
     }
 
     // Update is called once per frame
@@ -27,18 +60,24 @@ public class Move : MonoBehaviour
         if (Input.GetKeyDown(upKey))
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
+            spawnWall();
         }
         else if (Input.GetKeyDown(downKey))
         {
             GetComponent<Rigidbody2D>().velocity = -Vector2.up * speed;
+            spawnWall();
         }
         else if (Input.GetKeyDown(rightKey))
         {
             GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+            spawnWall();
         }
         else if (Input.GetKeyDown(leftKey))
         {
             GetComponent<Rigidbody2D>().velocity = -Vector2.right * speed;
+            spawnWall();
         }
+
+        fitColliderBetween(wall, lastWallEnd, transform.position);
     }
 }
